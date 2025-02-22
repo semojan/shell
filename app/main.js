81881100ff4d22d9e5a8291d2ba83d5b04fa24c5
semojan <97410424+semojan@users.noreply.github.com>
2025-02-22 18:31:41 +0330

    The type builtin: builtins

--- app/main.js
+++ app/main.js
@@ -10,21 +10,24 @@ const builtin = ["echo", "exit", "type"];
 
 function prompt() {
   rl.question("$ ", (answer) => {
-    const [command, text] = answer.split(" ");
-    console.log(text)
 
     if (answer === "exit 0") {
       exit(0);
     } else if (answer.startsWith("echo ")) {
+
       const text = answer.split("echo ");
       console.log(text[1]);
+
     } else if (answer.startsWith("type")) {
+      
       const command = answer.split("type ")[1];
+
       if (builtin.includes(command)){
         console.log(`${command} is a shell builtin`);
       } else {
         console.log(`${command}: not found`);
       }
+
     } else {
       console.log(`${answer}: command not found`);
     }
