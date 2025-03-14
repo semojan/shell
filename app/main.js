fdda40f7fa3d8012aa73c5774d17dc27fc59b031
semojan <97410424+semojan@users.noreply.github.com>
2025-03-14 22:20:45 +0330

    Multiple completions

--- app/main.js
+++ app/main.js
@@ -35,11 +35,10 @@ const rl = readline.createInterface({
 
     if (lastCompletion.count === 1) {
       process.stdout.write("\x07"); // Bell sound
-      console.log("")
+      console.log(" ")
       return [null, line.trim()];
     } else if (lastCompletion.count === 2) {
-      console.log(lastCompletion.hits)
-      console.log(lastCompletion.hits.join("  "));
+      console.log("\n" + lastCompletion.hits.join("  "));
       return [lastCompletion.hits, line.trim()];
     }
 
