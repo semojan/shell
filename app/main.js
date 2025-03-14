40c6a6f3713865c9652931aea12cea9fd3593c16
semojan <97410424+semojan@users.noreply.github.com>
2025-03-14 11:53:17 +0330

    Redirect stderr

--- app/main.js
+++ app/main.js
@@ -285,7 +285,7 @@ function prompt() {
       handleRedirect(errorMessage ? errorMessage : "", answer.split(" "), 2);
     }
 
-    if (redirect2 && result !== null) {
+    if (redirect2 && result !== "") {
       console.log(result);
     }
 
