e3909e39a51395c7c3c2a383360e8820434d3fda
semojan <97410424+semojan@users.noreply.github.com>
2025-03-14 13:30:42 +0330

    Append stdout

--- app/main.js
+++ app/main.js
@@ -193,7 +193,7 @@ function handleAppend(result, args, type, index) {
   if (index !== -1 && index + 1 < args.length) {
     const filePath = args[index + 1];
     try {
-      fsSync.writeFileSync(filePath, result + "\n", { flag: 'a' });
+      fs.writeFileSync(filePath, result + "\n", { flag: 'a' });
       return null;
     } catch (error) {
       return `${filePath}: No such file or directory`
