8cdd835fc49bcfd42fa73337e19ea07600fa7b64
semojan <97410424+semojan@users.noreply.github.com>
2025-03-16 09:46:32 +0330

    Multiple completions

--- app/main.js
+++ app/main.js
@@ -50,7 +50,6 @@ const rl = readline.createInterface({
     } else if (lastCompletion.count === 2) {
       console.log();
       console.log(hits.join("  "));
-      console.log("$ " + line);
       return [[], line];
     }
 
